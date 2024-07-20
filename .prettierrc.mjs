5b70555734e740898600275b5eac9717bf811afb
Ioram Gordadze <yoh0xff@Iorams-MacBook.local>
2024-07-21 00:53:49 +0400

    Init project

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+const config = {
+    bracketSpacing: true,
+    bracketSameLine: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    semi: true,
+    tabWidth: 2,
+    useTabs: false,
+    printWidth: 80,
+    arrowParens: 'always',
+    plugins: [
+        'prettier-plugin-organize-imports',
+        'prettier-plugin-packagejson',
+    ],
+};
+
+export default config;
