fb9119685b0b5b135afc5ddd14cad6241f87c1e2
Ioram Gordadaze <Yoh0xFF@gmail.com>
2024-07-21 01:03:33 +0400

    Read expression from stdin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import { createInterface } from 'readline';
+
+const reader = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function prompt() {
+  reader.question('Please enter the math expression:\n', (expression) => {
+    if (expression === 'quit') {
+      reader.close();
+      console.log('Goodbye!');
+    } else {
+      console.log('==> ', expression);
+      prompt();
+    }
+  });
+}
+
+prompt();
