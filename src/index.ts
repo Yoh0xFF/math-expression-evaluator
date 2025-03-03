b816347b7ba47402ecfab27513b710a3da4ed458
Ioram Gordadaze <Yoh0xFF@gmail.com>
2025-03-03 14:56:53 +0400

    Remove console logs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import { TerminalInput } from '@root/terminal/index.ts';
-import { config } from 'dotenv';
-
-config();
-console.log('=====> Lexer config: ', process.env['LEXER']);
-console.log('=====> Parser config: ', process.env['PARSER']);
 
 // Prompt
 const terminalInput = new TerminalInput();
